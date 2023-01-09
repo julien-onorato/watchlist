63e8ffa2a4e8efe52929a499f1bb693e7ed8fbb2
Julien Onorato <julien.onorato@gmail.com>
2023-01-09 11:57:54 +0100

    Initial Commit

---
+++ Watchlist/WatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchlistApp.swift
+//  Watchlist
+//
+//  Created by Julien Onorato on 09/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct WatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
